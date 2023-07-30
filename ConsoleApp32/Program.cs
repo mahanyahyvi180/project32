24dc04214f86c5b092a9d16bffaf2a63f00cb3e5
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-07-30 18:44:58 +0330

    Add files via upload

---
+++ ConsoleApp32/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var time = new Time1();
+
+            time.hour = 7;
+            time.minute = 15;
+            time.second = 30;
+
+            Console.ReadLine();
+        }
+    }
+}
